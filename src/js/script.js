50a84be592e058d7a2ff50d68b8ae9e0eedd08e4
Leof <leofff66@gmail.com>
2025-04-01 09:49:57 -0300

    adicionando operadores aritméticos e lógicos

--- src/js/script.js
+++ src/js/script.js
@@ -49,3 +49,40 @@ console.log(numfloat1.toString());
 
 let numInt=100;
 console.log(numInt.toString());
+
+
+//METODOS - PARTE 1
+
+//LENGTH - Verifica o tamanho da string
+
+let frase ="o mundo da Tecnologia";
+console.log(frase.length)
+
+//indexOf - retorna um trecho de um texto
+let texto = "Programação Sustentável"
+console.log(texto.indexOf("a"))
+
+//slice = retorna uma parte de um texto apontando o inicio e o final
+let info="processamento de ponta"
+console.log(info.slice(0,10))
+
+
+//Operadores aritméticos
+
+const num1 =10;
+const num2 =20;
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+//Operadores lógicos
+
+const num3 =20;
+const num4 =30;
+
+console.log(num3 < num4);
+console.log(num3 < num4 && num4 < 50) //Só se deve usar && (and/e) em questões verdadeiras
+console.log(num3 > num4 || num4 > num3) //Só se deve utilizar || (ou/or) quando pelo menos 1 seja verdadeiro 
+console.log(num3 == num4 || num4 <= num3) // == é comparação
