a1388d54f318c6cda363fb0d44e0429c404e5f32
Leof <leofff66@gmail.com>
2025-04-01 09:16:39 -0300

    Declaração e Variaveis

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome ="fiap";
+console.log(nome)
+
+let idade =21;
+console.log(idade)
+
+const sobrenome ="Cidade";
+console.log(sobrenome)
+//undefined
+let aula;
+console.log(aula)
+//null
+let valor = null;
+console.log(valor)
+
+let exemplo1={};
+
+let exemplo2=[];
+
+//TIPOS VARIAVEIS
+let exemplo3 = 10;
+console.log(typeof exemplo3)
+
+let exemplo4 = "Aula"
+console.log(typeof exemplo4)
+
+let exemplo5 =true;
+console.log(typeof exemplo5)
+
+let exemplo6 =["huguinho","zezinho","luizinho"]
+console.log(typeof exemplo6)
+
+
+//CONVERSÕES
